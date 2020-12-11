9820dbec9ef1c6ad0626cfcac8653005d1574df7
YunusEmreCMD <74190945+YunusEmreCMD@users.noreply.github.com>
2020-12-11 11:35:44 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+//var deButton = document.getElementById("hamburger-menu");
+//
+////function openMenu() {
+////  document.body.classList.toggle("menuOpen");
+////}
+////
+////deButton.addEventListener("click", openMenu);
+//
+//deButton.addEventListener("click", function() {
+//  document.body.classList.toggle("menuOpen");
+//});
+//
+//console.log("hoi");
+
+var deButton = document.getElementById("hamburger-menu");
+
+deButton.addEventListener("click", openMenu);
+
+function openMenu() {
+  document.body.classList.toggle("menuOpen");
+}
